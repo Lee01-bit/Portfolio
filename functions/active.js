94af5e745e94ee30ac3e673e277f2d075bc26811
lee01-bit <jaleellee19@gmail.com>
2024-11-27 21:56:33 +0200

    updated

--- functions/active.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('hamburgerToggle').addEventListener('click', () => {
-    const navbar = document.querySelector('.navbar');
-    const hamburger = document.querySelector('.hamburger');
-
-    // Toggle active state
-    navbar.classList.toggle('active');
-    hamburger.classList.toggle('active');
-});
